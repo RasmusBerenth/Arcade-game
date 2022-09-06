dfdfc7a27a606dc6ef5a662ea92110ed38aea77d
RasmusBerenth <98969083+RasmusBerenth@users.noreply.github.com>
2022-09-06 18:01:54 +0200

    Spawning, Physics and the player
    Created spawning proces
    started working on the physics (needs work)
    started working on the player controls

--- Arcade game/Assets/Scripts/SpawnManagment.cs
+++ Arcade game/Assets/Scripts/SpawnManagment.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class SpawnManagment : MonoBehaviour
 {
+    public GameObject[] environmentChunks;
+    public float startDelay = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //InvokeRepeating("SpawnChunks", startDelay, spawnInterval);
+        Invoke("SpawnChunks", startDelay);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void SpawnChunks()
+    {
+        int randomChunk = Random.Range(0, environmentChunks.Length);
+        Vector2 spawnPosition = new Vector2(70, -11);
+
+        Instantiate(environmentChunks[randomChunk], spawnPosition, environmentChunks[randomChunk].transform.rotation);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Respawn"))
+        {
+            Debug.Log("Triggerd");
+            SpawnChunks();
+        }
     }
 }
